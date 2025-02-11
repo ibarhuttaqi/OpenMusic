3f3167200d2b18420f0b359c5e528ba63d362a4b
ibarhuttaqi <ibarhuttaqi26@gmail.com>
2025-02-11 17:02:27 +0700

    feat: add user_album_likes table migration

---
+++ migrations/1739267816511_create-table-user-album-likes.js
@@ -0,0 +1,44 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('user_album_likes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint(
+    'user_album_likes',
+    'user_id',
+    'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE',
+  );
+
+  pgm.addConstraint(
+    'user_album_likes',
+    'album_id',
+    'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE',
+  );
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {};
