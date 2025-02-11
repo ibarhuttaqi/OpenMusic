6829711ca695e41488afccb281a506cebce3662b
ibarhuttaqi <ibarhuttaqi26@gmail.com>
2025-02-11 12:37:32 +0700

    feat: add export plugin, validator, and service using rabbitmq

---
+++ src/services/rabbitmq/ProducerService.js
@@ -0,0 +1,21 @@
+const amqp = require('amqplib');
+
+const ProducerService = {
+  sendMessage: async (queue, message) => {
+    const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(queue, {
+      durable: true,
+    });
+
+    channel.sendToQueue(queue, Buffer.from(message));
+    console.log(` [x] Sent ${message}`);
+
+    setTimeout(() => {
+      connection.close();
+    }, 1000);
+  },
+};
+
+module.exports = ProducerService;
